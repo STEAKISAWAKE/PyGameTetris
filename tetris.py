daa6fbf97b14f01f589b419f197dde7c8951b046
Steak <cadenmillerstudios@gmail.com>
2020-03-05 11:55:18 -0500

    delta

--- tetris.py
+++ tetris.py
@@ -9,24 +9,32 @@ import sys, pygame
 pygame.init()
 
 size = width, height = 1000, 900
-speed = [1, 1]
+speed = [0, 1]
+pos = [0.0, 0.0]
 black = 0, 0, 0
 
 screen = pygame.display.set_mode(size)
 
-block = pygame.image.load("");
+block = pygame.image.load("square.png");
 blockrect = block.get_rect()
 
+clock = pygame.time.Clock();
+previousTime = 0
+newTime = clock.get_time()
+
 while 1:
+    previousTime = newTime
+    newTime = clock.get_time()
+
+    deltatime = previousTime - newTime
+
     for event in pygame.event.get():
         if event.type == pygame.QUIT: sys.exit()
 
-    ballrect = ballrect.move(speed)
-    if ballrect.left < 0 or ballrect.right > width:
-        speed[0] = -speed[0]
-    if ballrect.top < 0 or ballrect.bottom > height:
-        speed[1] = -speed[1]
+    pos = [1, int(100 * deltatime)]
+
+    blockrect = blockrect.move(pos)
 
     screen.fill(black)
-    screen.blit(ball, ballrect)
+    screen.blit(block, blockrect)
     pygame.display.flip()
