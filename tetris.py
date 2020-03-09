733564592f85b7529f29883a36d22296ef9ac3a6
Steak <33386230+STEAKISAWAKE@users.noreply.github.com>
2020-03-09 11:57:09 -0400

    Add files via upload

--- tetris.py
+++ tetris.py
@@ -5,36 +5,51 @@ Created on Wed Mar  4 11:29:33 2020
 @author: miller.caden
 """
 
-import sys, pygame
+import pygame
 pygame.init()
 
 size = width, height = 1000, 900
-speed = [0, 1]
-pos = [0.0, 0.0]
+step = 32
+speed = [step, step]
+pos = [100, 0]
 black = 0, 0, 0
+
 
 screen = pygame.display.set_mode(size)
 
-block = pygame.image.load("square.png");
+block = pygame.image.load("square.png")
 blockrect = block.get_rect()
 
-clock = pygame.time.Clock();
-previousTime = 0
-newTime = clock.get_time()
+getTicksLastFrame = 0.0
+timeCheck = (pygame.time.get_ticks()) / 1000.0
 
 while 1:
-    previousTime = newTime
-    newTime = clock.get_time()
-
-    deltatime = previousTime - newTime
+    t = pygame.time.get_ticks()
+    # deltaTime in seconds.
+    deltaTime = (t - getTicksLastFrame) / 1000.0
+    getTicksLastFrame = t
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: sys.exit()
+        if event.type == pygame.QUIT: 
+            pygame.quit()
 
-    pos = [1, int(100 * deltatime)]
 
-    blockrect = blockrect.move(pos)
 
+
+    speed[0] = 1 + speed[0] * (deltaTime * deltaTime)
+
+    pos = [pos[0], speed[1] * deltaTime + pos[1]]
+
+
+
+    print(pos)
+
+    blockrect.center = (int(pos[0]), int(pos[1]))
+    
     screen.fill(black)
     screen.blit(block, blockrect)
     pygame.display.flip()
+    
+
+
+
