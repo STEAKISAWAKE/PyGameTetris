95b3d18abd98fa60f380aa229e86c54b3a1ee08b
Steak <33386230+STEAKISAWAKE@users.noreply.github.com>
2020-03-10 11:56:20 -0400

    Add files via upload

--- tetris.py
+++ tetris.py
@@ -8,12 +8,11 @@ Created on Wed Mar  4 11:29:33 2020
 import pygame
 pygame.init()
 
-size = width, height = 1000, 900
+size = width, height = 1032, 932
 step = 32
 speed = [step, step]
-pos = [100, 0]
+pos = [width / 2, 16]
 black = 0, 0, 0
-
 
 screen = pygame.display.set_mode(size)
 
@@ -23,6 +22,8 @@ blockrect = block.get_rect()
 getTicksLastFrame = 0.0
 timeCheck = (pygame.time.get_ticks()) / 1000.0
 
+timeSinceLastMove = 0.0
+
 while 1:
     t = pygame.time.get_ticks()
     # deltaTime in seconds.
@@ -30,15 +31,24 @@ while 1:
     getTicksLastFrame = t
 
     for event in pygame.event.get():
-        if event.type == pygame.QUIT: 
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_DOWN:
+                pos = [pos[0], speed[1] + pos[1]]
+            if event.key == pygame.K_LEFT:
+                pos = [pos[0] - speed[0], pos[1]]
+            if event.key == pygame.K_RIGHT:
+                pos = [speed[0] + pos[0], pos[1]]
+
+        if event.type == pygame.QUIT:
             pygame.quit()
 
+# So that the blocks auto move when one second passes
+    if (t - timeSinceLastMove) / 1000.0 >= 1.0:
+        timeSinceLastMove = t
+        pos = [pos[0], speed[1] + pos[1]]
 
 
 
-    speed[0] = 1 + speed[0] * (deltaTime * deltaTime)
-
-    pos = [pos[0], speed[1] * deltaTime + pos[1]]
 
 
 
@@ -47,7 +57,7 @@ while 1:
     blockrect.center = (int(pos[0]), int(pos[1]))
     
     screen.fill(black)
-    screen.blit(block, blockrect)
+    screen.blit(block, blockrect, (1.0, 0.0, 0.0, 1.0), special_flags=pygame.BLEND_RGBA_ADD)
     pygame.display.flip()
     
 
